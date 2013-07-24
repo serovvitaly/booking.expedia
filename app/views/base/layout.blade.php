835eb527490299275739d236414efc2435850212
Vitaly Serov <serovvitaly@gmail.com>
2013-07-24 22:12:01 +0400

    очередной коммит

---
+++ app/views/base/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Untitled</title>
+  <link rel="stylesheet" type="text/css" href="/packages/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="span12">Верхнее меню</div>
+    </div>
+  </div>
+</body>
+</html>
